7f284f009f8a87be2c1a56557701d456e5cf3c74
elbeicktalat <talatelbeick@gmail.com>
2021-11-23 15:59:15 +0100

    add sections in home

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,59 @@
 import React from 'react';
-import "./App.css";
+import "./Home.css";
 import styled from "styled-components";
+import Section from "../section/Section";
 
 function Home(): JSX.Element {
     return (
         <Container className="Home">
-            Home works fine!
+                <Section
+                    title="Model S"
+                    subTitle="Order Online for Touchless Delivery"
+                    backgroundUrl="model-s.jpg"
+                    firstButtonText="custom order"
+                    secondButtonText="Existing inventory"
+                    isFirst
+                />
+                <Section
+                    title="Model Y"
+                    subTitle="Order Online for Touchless Delivery"
+                    backgroundUrl="model-y.jpg"
+                    firstButtonText="custom order"
+                    secondButtonText="Existing inventory"
+                />
+                <Section
+                    title="Model 3"
+                    subTitle="Order Online for Touchless Delivery"
+                    backgroundUrl="model-3.jpg"
+                    firstButtonText="custom order"
+                    secondButtonText="Existing inventory"
+                />
+                <Section
+                    title="Model X"
+                    subTitle="Order Online for Touchless Delivery"
+                    backgroundUrl="model-x.jpg"
+                    firstButtonText="custom order"
+                    secondButtonText="Existing inventory"
+                />
+                <Section
+                    title="Solar Panels"
+                    subTitle="Lowest Cost Solar Panels in America"
+                    backgroundUrl="solar-panels.jpg"
+                    firstButtonText="Order now"
+                    secondButtonText="Learn more"
+                />
+                <Section
+                    title="Solar Roof"
+                    subTitle="Produce Clean Energy From Your Roof"
+                    backgroundUrl="solar-roof.jpg"
+                    firstButtonText="Order now"
+                    secondButtonText="Learn more"
+                />
+                <Section
+                    title="Accessories"
+                    backgroundUrl="accessories.jpg"
+                    firstButtonText="Shop now"
+                />
         </Container>
     );
 }
