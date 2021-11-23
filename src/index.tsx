f823412c8978b6ebdf2b32a02ad78ad5ce587f49
elbeicktalat <talatelbeick@gmail.com>
2021-11-23 13:44:41 +0100

    add gotham font

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "./fonts/gotham/GothamMedium.ttf";
+import "./fonts/gotham/GothamBook.ttf";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
