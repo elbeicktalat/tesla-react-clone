154ff34afb771da440fc755852ba6a9efe855cb5
elbeicktalat <talatelbeick@gmail.com>
2021-11-24 09:19:22 +0100

    use link prop

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,39 +8,43 @@ function Home(): JSX.Element {
         <Container className="Home">
                 <Section
                     title="Model S"
-                    subTitle="Order Online for Touchless Delivery"
+                    subTitle="Order Online for"
+                    subTitleLinkText="Touchless Delivery"
                     backgroundUrl="model-s.jpg"
                     firstButtonText="custom order"
                     secondButtonText="Existing inventory"
                     isFirst
                 />
-                <Section
-                    title="Model Y"
-                    subTitle="Order Online for Touchless Delivery"
-                    backgroundUrl="model-y.jpg"
-                    firstButtonText="custom order"
-                    secondButtonText="Existing inventory"
-                />
-                <Section
-                    title="Model 3"
-                    subTitle="Order Online for Touchless Delivery"
-                    backgroundUrl="model-3.jpg"
-                    firstButtonText="custom order"
-                    secondButtonText="Existing inventory"
-                />
-                <Section
-                    title="Model X"
-                    subTitle="Order Online for Touchless Delivery"
-                    backgroundUrl="model-x.jpg"
-                    firstButtonText="custom order"
-                    secondButtonText="Existing inventory"
-                />
-                <Section
-                    title="Solar Panels"
-                    subTitle="Lowest Cost Solar Panels in America"
-                    backgroundUrl="solar-panels.jpg"
-                    firstButtonText="Order now"
-                    secondButtonText="Learn more"
+            <Section
+                title="Model Y"
+                subTitle="Order Online for"
+                subTitleLinkText="Touchless Delivery"
+                backgroundUrl="model-y.jpg"
+                firstButtonText="custom order"
+                secondButtonText="Existing inventory"
+            />
+            <Section
+                title="Model 3"
+                subTitle="Order Online for"
+                subTitleLinkText="Touchless Delivery"
+                backgroundUrl="model-3.jpg"
+                firstButtonText="custom order"
+                secondButtonText="Existing inventory"
+            />
+            <Section
+                title="Model X"
+                subTitle="Order Online for"
+                subTitleLinkText="Touchless Delivery"
+                backgroundUrl="model-x.jpg"
+                firstButtonText="custom order"
+                secondButtonText="Existing inventory"
+            />
+            <Section
+                title="Solar Panels"
+                subTitle="Lowest Cost Solar Panels in America"
+                backgroundUrl="solar-panels.jpg"
+                firstButtonText="Order now"
+                secondButtonText="Learn more"
                 />
                 <Section
                     title="Solar Roof"
